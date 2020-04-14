6b7250acc60519d69192aa60bc38f9decb0b55be
marta-garcer <m.garciace.2017@qlumnos.urjc.es>
2020-04-14 18:50:30 +0200

    done!!!

---
+++ P2/Ex4.py
@@ -0,0 +1,17 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 4
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "192.168.1.58"
+
+PORT = 8086
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+c.debug_talk("Message 1---", 'green')
+c.debug_talk("Message 2: Testing !!!", 'green')
---
+++ P2/Ex5.py
@@ -0,0 +1,22 @@
+from Client0 import Client
+from Seq1 import Seq
+
+PRACTICE = 2
+EXERCISE = 5
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters
+IP = "10.3.33.54"
+PORT = 8086
+FOLDER = "../Session-04/"
+filename = FOLDER + 'U5.txt'
+s = Seq()
+s.read_fasta(filename)
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+c.debug_talk("Sending the U5 Gene to the server...", 'blue')
+
+c.debug_talk(s, 'blue')
